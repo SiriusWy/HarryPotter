5e424b57f976a162c43258a4131b35619dc4f8dd
SiriusWy <xianwangyue123@163.com>
2019-02-01 10:34:28 +0800

    Signed-off-by: SiriusWy <xianwangyue123@163.com>

--- src/main/java/HarryPotter.java
+++ src/main/java/HarryPotter.java
@@ -1,4 +1,17 @@
 public class HarryPotter {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    int age;
+
+    public HarryPotter() {
+    }
+
     public static void main(String[] args) {
         System.out.println("this is harry potter");
     }
